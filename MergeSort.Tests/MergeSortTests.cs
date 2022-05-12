d4c16cad57919e68dc7a9ca123573c8e55daef99
Mo Sen <muhammedsen@gmail.com>
2022-05-12 11:49:10 +0300

    test added

--- MergeSort.Tests/MergeSortTests.cs
+++ MergeSort.Tests/MergeSortTests.cs
@@ -12,34 +12,15 @@ namespace MergeSort.Tests
         {
             var result = MergeSort.Program.MergeSort(unsorted);
             Assert.Equal(expectedValue,result);
-
         }
-
-        [Theory]
-        [MemberData(nameof(ArrayData))]
-        public void MergeSortWithArray_Order_Test(int[] unsorted, int[] expectedValue)
-        {
-            var result = MergeSort.Program.MergeSortWithArray(unsorted);
-            Assert.Equal(expectedValue, result);
-
-        }
-
         public static IEnumerable<object[]> Data {
             get
             {
                 yield return new object[] { new List<int> { 600, 42, 2112 }, new List<int> { 42, 600, 2112 } };
                 yield return new object[] { new List<int> { 600, 42, 2112, 1000, 950 }, new List<int> { 42, 600, 950, 1000, 2112 } };
                 yield return new object[] { new List<int> { 300, 600, 42, 2112 }, new List<int> { 42, 300, 600, 2112 } };
-            }
-        }
+                yield return new object[] { new List<int> { 200, 100, 12, 1112 }, new List<int> { 12, 300, 200, 1112 } };
 
-        public static IEnumerable<object[]> ArrayData
-        {
-            get
-            {
-                yield return new object[] { new int[] { 600, 42, 2112 }, new int[] { 42, 600, 2112 } };
-                yield return new object[] { new int[] { 600, 42, 2112, 1000, 950 }, new int[] { 42, 600, 950, 1000, 2112 } };
-                yield return new object[] { new int[] { 300, 600, 42, 2112 }, new int[] { 42, 300, 600, 2112 } };
             }
         }
 
--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -13,35 +13,19 @@ namespace MergeSort
             
             Random random = new Random();
             var sizeOfArray = random.Next(10, 100);
-            int[] arrayUnsorted = new int[sizeOfArray];
-            int[] arraySorted = new int[sizeOfArray];
-
+           
             Console.WriteLine("Original List elements:");
             for (int i = 0; i < sizeOfArray; i++)
             {
                 unsorted.Add(random.Next(0, 100));
-                arrayUnsorted[i] = random.Next(0, 100);
                 Console.Write(unsorted[i] + " ");
             }
             Console.WriteLine();
 
             sorted = MergeSort(unsorted);
-            arraySorted = MergeSortWithArray(arrayUnsorted);
+
             Console.WriteLine("Sorted List elements: ");
             foreach (int x in sorted)
-            {
-                Console.Write(x + " ");
-            }
-            Console.Write("\n\n");
-
-            Console.WriteLine("Original Array elements:");
-            foreach (int x in arrayUnsorted)
-            {
-                Console.Write(x + " ");
-            }
-            Console.Write("\n");
-            Console.WriteLine("Sorted Array elements: ");
-            foreach (int x in arraySorted)
             {
                 Console.Write(x + " ");
             }
@@ -124,97 +108,5 @@ namespace MergeSort
             }
             return result;
         }
-
-        public static int[] MergeSortWithArray(int[] array)
-        {
-            int[] left;
-            int[] right;
-
-            // return single element array
-            if (array.Length <= 1)
-                return array;
-            // The  middle of our array  
-            int middle = array.Length / 2;
-            //Will represent our 'left' array
-            left = new int[middle];
-
-            //Will represent our 'right' array
-            right = new int[array.Length - middle];
-          
-            //populate left array
-            for (int i = 0; i < middle; i++)
-                left[i] = array[i];
-            
-            //populate right array   
-            int x = 0;
-            //We start our index from the middle, as we have already populated the left array from 0 to midpont
-            for (int i = middle; i < array.Length; i++)
-            {
-                right[x] = array[i];
-                x++;
-            }
-            //Recursively sort the left array
-            left = MergeSortWithArray(left);
-            //Recursively sort the right array
-            right = MergeSortWithArray(right);
-            //Merge our two sorted arrays
-            
-            return MergeWithArray(left, right); ;
-        }
-
-        public static int[] MergeWithArray(int[] left, int[] right)
-        {
-            int resultLength = right.Length + left.Length;
-            int[] result = new int[resultLength];
-            //
-            int indexLeft = 0, indexRight = 0, indexResult = 0;
-            //while either array still has an element
-            while (indexLeft < left.Length || indexRight < right.Length)
-            {
-                //if both arrays have elements  
-                if (indexLeft < left.Length && indexRight < right.Length)
-                {
-                    //If item on left array is less than item on right array, add that item to the result array 
-                    if (left[indexLeft] <= right[indexRight])
-                    {
-                        result[indexResult] = left[indexLeft];
-                        indexLeft++;
-                        indexResult++;
-                    }
-                    // else the item in the right array wll be added to the results array
-                    else
-                    {
-                        result[indexResult] = right[indexRight];
-                        indexRight++;
-                        indexResult++;
-                    }
-                }
-                //if only the left array still has elements, add all its items to the results array
-                else if (indexLeft < left.Length)
-                {
-                    while (indexLeft < left.Length)
-                    {
-                        result[indexResult] = left[indexLeft];
-                        // iterate indexLeft to the length of left array
-                        indexLeft++;
-                        indexResult++;
-                    }
-                }
-                //if only the right array still has elements, add all its items to the results array
-                else if (indexRight < right.Length)
-                {
-                    while (indexRight < right.Length)
-                    {
-                        result[indexResult] = right[indexRight];
-                        // iterate indexright to the length of right array
-                        indexRight++;
-                        indexResult++;
-                    }
-                }
-            }
-            return result;
-        }
-
-
     }
 }
